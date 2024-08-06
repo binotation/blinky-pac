85720e4798313d8442cf125047144cd90d7b3167
binotation <68807919+binotation@users.noreply.github.com>
2024-08-06 16:55:27 +1000

    Improve comments

--- src/main.rs
+++ src/main.rs
@@ -33,18 +33,19 @@ fn main() -> ! {
 
     let dp = stm32l4x2::Peripherals::take().unwrap();
 
-    // Enable peripheral clocks
+    // Enable peripheral clocks - GPIOB, TIM2
     dp.RCC.ahb2enr.write(|w| w.gpioben().set_bit());
     dp.RCC.apb1enr1.write(|w| w.tim2en().set_bit());
 
-    // Configure GPIOB as push-pull output
+    // Configure B6 as push-pull output
     dp.GPIOB.moder.write(|w| w.moder6().output());
     dp.GPIOB.otyper.write(|w| w.ot6().push_pull());
 
     // Set TIM2 to 1Hz
     dp.TIM2.psc.write(|w| w.psc().bits(399)); // 4Mhz / 400 = 10KHz
     dp.TIM2.arr.write(|w| w.arr().bits(9999)); // 10KHz / 10000 = 1Hz
-                                               // Enable TIM2 update interrupt
+
+    // Enable TIM2 update interrupt
     dp.TIM2.dier.write(|w| w.uie().set_bit());
 
     // Unmask TIM2 global interrupt
