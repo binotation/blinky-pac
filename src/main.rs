e0d12ddc7052ad4d2324e4dc9c91e16fdad7bd68
binotation <68807919+binotation@users.noreply.github.com>
2024-09-01 13:06:03 +1000

    u575 blinky

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,34 @@
 
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
                      // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
-                     // use cortex_m;
-use cortex_m_rt::entry;
-use stm32l4::stm32l4x2::{self, interrupt};
 
-static mut TIM2_PERIPHERAL: Option<stm32l4x2::TIM2> = None;
-static mut GPIOB_PERIPHERAL: Option<stm32l4x2::GPIOB> = None;
+use cortex_m_rt::entry;
+use stm32u5::stm32u575::{interrupt, Interrupt, Peripherals, GPIOB, GPIOC, TIM2};
+
+static mut TIM2_PERIPHERAL: Option<TIM2> = None;
+static mut GPIOB_PERIPHERAL: Option<GPIOB> = None;
+static mut GPIOC_PERIPHERAL: Option<GPIOC> = None;
 
 #[interrupt]
 fn TIM2() {
     // SAFETY: impossible race condition
     if let Some(tim2) = unsafe { TIM2_PERIPHERAL.as_mut() } {
-        if tim2.sr.read().uif().bit_is_set() {
+        if tim2.sr().read().uif().bit_is_set() {
             if let Some(gpiob) = unsafe { GPIOB_PERIPHERAL.as_ref() } {
-                if gpiob.odr.read().odr6().bit_is_set() {
-                    gpiob.bsrr.write(|w| w.br6().reset());
+                if gpiob.odr().read().od7().bit_is_set() {
+                    gpiob.bsrr().write(|w| w.br7().reset());
                 } else {
-                    gpiob.bsrr.write(|w| w.bs6().set());
+                    gpiob.bsrr().write(|w| w.bs7().set_());
                 }
             }
-            tim2.sr.write(|w| w.uif().clear_bit())
+            if let Some(gpioc) = unsafe { GPIOC_PERIPHERAL.as_ref() } {
+                if gpioc.odr().read().od7().bit_is_set() {
+                    gpioc.bsrr().write(|w| w.br7().reset());
+                } else {
+                    gpioc.bsrr().write(|w| w.bs7().set_());
+                }
+            }
+            tim2.sr().write(|w| w.uif().clear_bit())
         }
     }
 }
@@ -31,32 +39,38 @@ fn TIM2() {
 fn main() -> ! {
     // Device defaults to 4MHz clock
 
-    let dp = stm32l4x2::Peripherals::take().unwrap();
+    let dp = Peripherals::take().unwrap();
 
-    // Enable peripheral clocks - GPIOB, TIM2
-    dp.RCC.ahb2enr.write(|w| w.gpioben().set_bit());
-    dp.RCC.apb1enr1.write(|w| w.tim2en().set_bit());
+    // Enable peripheral clocks - GPIOB, GPIOC, TIM2
+    dp.RCC
+        .ahb2enr1()
+        .write(|w| w.gpioben().enabled().gpiocen().enabled());
+    dp.RCC.apb1enr1().write(|w| w.tim2en().enabled());
 
-    // Configure B6 as push-pull output
-    dp.GPIOB.moder.write(|w| w.moder6().output());
-    dp.GPIOB.otyper.write(|w| w.ot6().push_pull());
+    // Configure B7 (LED 2), C7 (LED 1) as push-pull output
+    dp.GPIOB.moder().write(|w| w.mode7().output());
+    dp.GPIOB.otyper().write(|w| w.ot7().push_pull());
+    dp.GPIOB.bsrr().write(|w| w.bs7().set_()); // Set LED 2 initially
+    dp.GPIOC.moder().write(|w| w.mode7().output());
+    dp.GPIOC.otyper().write(|w| w.ot7().push_pull());
 
     // Set TIM2 to 1Hz
-    dp.TIM2.psc.write(|w| w.psc().bits(399)); // 4Mhz / 400 = 10KHz
-    dp.TIM2.arr.write(|w| w.arr().bits(9999)); // 10KHz / 10000 = 1Hz
+    dp.TIM2.psc().write(|w| unsafe { w.bits(399) }); // 4Mhz / 400 = 10KHz
+    dp.TIM2.arr().write(|w| unsafe { w.bits(9999) }); // 10KHz / 10000 = 1Hz
 
     // Enable TIM2 update interrupt
-    dp.TIM2.dier.write(|w| w.uie().set_bit());
+    dp.TIM2.dier().write(|w| w.uie().set_bit());
 
     // Unmask TIM2 global interrupt
-    unsafe { cortex_m::peripheral::NVIC::unmask(stm32l4x2::Interrupt::TIM2) }
+    unsafe { cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2) }
 
     // Enable TIM2 counter
-    dp.TIM2.cr1.write(|w| w.cen().set_bit());
+    dp.TIM2.cr1().write(|w| w.cen().set_bit());
 
     unsafe {
         TIM2_PERIPHERAL = Some(dp.TIM2);
         GPIOB_PERIPHERAL = Some(dp.GPIOB);
+        GPIOC_PERIPHERAL = Some(dp.GPIOC);
     }
 
     #[allow(clippy::empty_loop)]
